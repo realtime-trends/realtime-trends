0ae83a60ebf542ecaacd0ed0ea1f689a997b8b11
Changho Park <hoya.develop@gmail.com>
2022-04-07 07:45:59 +0900

    chore: Add google meta tag.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
 import React from 'react';
 import Document, {Html, Head, Main, NextScript} from 'next/document';
@@ -8,6 +9,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
+          <meta name="google-site-verification" content="54SG3TNIx0BzNeZ1-XFsHzAM09nR1-K4X1_MjwaXLDY" />
         </Head>
         <body>
           <Main />
