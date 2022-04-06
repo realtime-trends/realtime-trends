48602ab11001de430110e5bc141c7815239568be
Changho Park <hoya.develop@gmail.com>
2022-04-07 07:25:43 +0900

    fix: Remove footer.

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ const Home: NextPage<propsType> = ({trends}: propsType) => {
         <ChartBox trends={trends} isNaverSection={false}/>
       </main>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -46,7 +46,7 @@ const Home: NextPage<propsType> = ({trends}: propsType) => {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 };
