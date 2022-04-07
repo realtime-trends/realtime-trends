2a4ccf234a95dfd5a305def2fd993340118cbd6f
Changho Park <hoya.develop@gmail.com>
2022-04-07 17:44:34 +0000

    Deploy Next.js to gh-pages

---
+++ _next/static/hm2Seodz8h5Gw59U6khhe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/807-313fa7ba62440c86.js","static/chunks/pages/index-bbd0d999082e87e8.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
