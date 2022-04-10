ec434394a32ef636f6964a40a2286b102eaa61a7
Changho Park <hoya.develop@gmail.com>
2022-04-10 13:12:40 +0900

    Update: function.

---
+++ netlify/functions/run-updater-ga.ts
@@ -0,0 +1,25 @@
+/* eslint-disable max-len */
+import {Handler} from '@netlify/functions';
+import {Octokit} from '@octokit/core';
+
+const handler: Handler = async (event, context) => {
+  const octokit = new Octokit();
+
+  await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+    owner: 'realtime-trends',
+    repo: 'realtime-trends-updater',
+    workflow_id: 23875462,
+    ref: 'main',
+    inputs: {
+      branch: 'data',
+      key: '0',
+    },
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({message: 'Success'}),
+  };
+};
+
+export {handler};
