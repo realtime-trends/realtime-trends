19117e3d45245ecfb779347232b0a59e9f72db48
Changho Park <hoya.develop@gmail.com>
2022-04-10 16:53:33 +0900

    feat: Add naver gray button.

--- components/naver-openmain-button.tsx
+++ components/naver-openmain-button.tsx
@@ -11,19 +11,16 @@ interface PropsType {
 
 const NaverOpenmainButton = function({isNaverSection}: PropsType) {
   const [scriptLoaded, setScriptLoaded] = useState(false);
-  const [elementLoaded, setElementLoaded] = useState(false);
 
   useEffect(() => {
-    if (!elementLoaded) {
+    if (!scriptLoaded) {
       loadOpenmain(() => {
         if (!isNaverSection) {
-          setScriptLoaded(true);
-
           const hiddenOpenmainElement = document.getElementById('hidden-openmain');
           if (hiddenOpenmainElement) {
             const openmainDiv = hiddenOpenmainElement.getElementsByTagName('div')[0];
             if (openmainDiv) {
-              setElementLoaded(true);
+              setScriptLoaded(true);
             }
           }
         }
@@ -31,30 +28,35 @@ const NaverOpenmainButton = function({isNaverSection}: PropsType) {
     }
   });
 
-  if (elementLoaded) {
-    return (
-      <>
-        <div id="hidden-openmain" className="hidden nv-openmain" data-title="실시간검색어" data-type="W2"></div>
-        <div className="grid grid-cols-5 p-5 m-2 md:hidden bg-gray-100 md:hover:bg-blue-300" onClick={() => {
+  return (
+    <>
+      <div id="hidden-openmain" className="hidden nv-openmain" data-title="실시간검색어" data-type="W2"></div>
+      <div className="grid grid-cols-5 p-5 m-2 md:hidden bg-gray-100 md:hover:bg-blue-300" onClick={() => {
+        if (scriptLoaded) {
           const element = document.querySelector('#hidden-openmain > div > button') as HTMLElement;
           element.click();
-        }}>
-          <div className='flex justify-start items-center'><Image src="/naver.svg" height={32} width={32} /></div>
-          <div className='col-span-4 flex justify-end items-center'>
+        }
+      }}>
+        <div className='flex justify-start items-center'>
+          { (scriptLoaded) ?
+            <Image src="/naver.svg" height={32} width={32} /> :
+            <Image src="/naver-gray.svg" height={32} width={32} />
+          }
+
+        </div>
+        <div className='col-span-4 flex justify-end items-center'>
+          { (scriptLoaded) ?
             <span>
               네이버 메인에&nbsp;<span className='text-green-500'>#실시간검색어</span>&nbsp;추가
+            </span> :
+            <span>
+              <span className='text-gray-500'>네이버 메인에 #실시간검색어 추가</span>
             </span>
-          </div>
+          }
         </div>
-      </>
-    );
-  } else if (scriptLoaded) {
-    return (
-      <div id="hidden-openmain" className="hidden nv-openmain" data-title="실시간검색어" data-type="W2"></div>
-    );
-  } else {
-    return (<div></div>);
-  }
+      </div>
+    </>
+  );
 };
 
 export default NaverOpenmainButton;
