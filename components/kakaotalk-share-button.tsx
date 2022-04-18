ab835344bf1c70f43c29f62706b6e4ec840a67e2
Changho Park <hoya.develop@gmail.com>
2022-04-18 19:57:27 +0900

    Update kakaotalk-share-button.tsx

--- components/kakaotalk-share-button.tsx
+++ components/kakaotalk-share-button.tsx
@@ -18,7 +18,7 @@ const KakaotalkShareButton = () => {
         <Image src="/kakaotalk.svg" height={32} width={32} />
       </div>
       <div className='col-span-4 flex justify-end items-center break-normal'>
-        <span>카카오톡으로&nbsp;<span className='text-yellow-700'>#실시간검색어</span>&nbsp;공유</span>
+        <span>카카오톡으로&nbsp;<span className='text-yellow-700'>리얼타임 실검</span>&nbsp;공유</span>
       </div>
     </div>
   );
