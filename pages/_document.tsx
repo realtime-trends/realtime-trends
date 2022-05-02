3d18d025b17ee18fe0274cd1dcc4b128dc47f758
Changho Park <hoya.develop@gmail.com>
2022-05-02 11:34:52 +0900

    feat: add adsense

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,18 +13,30 @@ class MyDocument extends Document {
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           />
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8196967081193084"
-            crossOrigin="anonymous"></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-          page_path: window.location.pathname,
-        });
-      `,
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+           />
+           <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (adsbygoogle = window.adsbygoogle || []).push({
+                  google_ad_client: '${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID}',
+                  enable_page_level_ads: true
+                });
+              `
             }}
           />
         </Head>
